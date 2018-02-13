56bf9f0180c3d69b7d2218111d3cd7194d88c45f
Henry Vuong <henryvuong@Henrys-Air.kadirnet.com>
2018-02-12 23:42:11 -0600

    Cleaned up code

--- Week1Lab-TumblrFeed/PhotoCell.swift
+++ Week1Lab-TumblrFeed/PhotoCell.swift
@@ -13,13 +13,10 @@ class PhotoCell: UITableViewCell {
     @IBOutlet weak var cellView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
--- Week1Lab-TumblrFeed/View Controllers/PhotosViewController.swift
+++ Week1Lab-TumblrFeed/View Controllers/PhotosViewController.swift
@@ -112,17 +112,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
                     // Tell the refreshControl to stop spinning
                     refreshControl.endRefreshing()
                 }
-        
             }
             task.resume()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
